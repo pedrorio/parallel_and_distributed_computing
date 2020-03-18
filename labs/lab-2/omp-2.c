9d725a8d57395f38fa1d81e200178bcdd36275d6
Pedro Rio <pedrocacaisrio@gmail.com>
2020-03-18 15:35:00 +0000

    🚧 wip: Class 2️

---
+++ labs/lab-2/omp-2.c
@@ -0,0 +1,66 @@
+/*
+DESCRIPTION:
+		Parallelizing an inner loop with dependences
+
+		for (iter=0; iter<numiter; iter++) {
+			for (i=0; i<size-1; i++) {
+				V[i] = f( V[i], V[i+1] );
+			}
+		}
+
+**************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define TOTALSIZE 1000
+#define NUMITER 200
+
+/*
+* DUMMY FUNCTION
+*/
+#define f(x, y)    ((x+y)/2.0)
+
+
+/* MAIN: PROCESS PARAMETERS */
+int main(int argc, char *argv[]) {
+
+    /* VARIABLES */
+    int i, iter, tid;
+
+    /* DECLARE VECTOR AND AUX DATA STRUCTURES */
+    double *V = (double *) malloc(TOTALSIZE * sizeof(double));
+    double *C = (double *) malloc(TOTALSIZE * sizeof(double));
+
+    /* 1. INITIALIZE VECTOR */
+    #pragma omp parallel for
+    for (i = 0; i < TOTALSIZE; i++) {
+        V[i] = 0.0 + i;
+    }
+
+    C = V;
+
+    /* 2. ITERATIONS LOOP */
+        #pragma omp parallel for collapse(2) private(iter, i, tid)
+        for (int iter = 0; iter < NUMITER; iter++) {
+
+            /* 2.1. PROCESS ELEMENTS */
+//            Fix the parallel version by copying vector V from iteration i-1 before executing
+//            iteration i, and using the copied values as arguments to function f in iteration i.
+//            #pragma omp parallel for private(iter, i, tid)
+            for (int i = 0; i < TOTALSIZE - 1; i++) {
+                V[i] = f(V[i], V[i + 1]);
+            }
+
+//            C = V;
+            /* 2.2. END ITERATIONS LOOP */
+        }
+
+
+    /* 3. OUTPUT FINAL VALUES */
+    printf("Output:\n");
+    for (i = 0; i < TOTALSIZE; i++) {
+        printf("%4d %f\n", i, V[i]);
+    }
+
+}
