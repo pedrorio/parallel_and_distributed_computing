79cdac743af65a7ca53ef0f1b84e1120b593938a
Pedro Rio <pedrocacaisrio@gmail.com>
2020-05-12 23:22:34 +0100

    🚧 wip: Back to basics

--- mpi-c/src/readInput.h
+++ mpi-c/src/readInput.h
@@ -16,60 +16,86 @@ void readInput(const char inputFileName[], const int processId, const int number
     double *MatrixA;
     int *nonZeroElementIndexes;
 
+    // returned in the struct
     int numberOfIterations;
     double convergenceCoefficient;
     int numberOfFeatures;
 
+    // used in this function
     int numberOfUsers;
     int numberOfItems;
     int numberOfNonZeroElements;
 
-
     const char space[2] = " ";
     char *token;
-//
+
     int maxDoubleSize = snprintf(NULL, 0, "%d", (int) DBL_MAX);
     int maxLineSize = 3 * maxDoubleSize + 2 + 1;
-//    char line[maxLineSize];
+    char line[maxLineSize];
 
-    struct fileCopy *fc;
+    int totalNumberOfLines = 0;
+    FILE *inputFile;
 
+    inputFile = fopen(inputFileName, "r");
+    if (inputFile) {
+        while (fgets(line, maxLineSize, inputFile) != NULL) {
+            totalNumberOfLines++;
+        }
+        fclose(inputFile);
+    }
 
-    fc = createFileCopy(fc, inputFileName);
+    char *fileCopy = malloc(totalNumberOfLines * maxLineSize * sizeof(char));
+
+    int lineNumber = 0;
+    inputFile = fopen(inputFileName, "r");
+    if (inputFile) {
+        while (fgets(line, maxLineSize, inputFile) != NULL) {
+            for (int j = 0; j <= maxLineSize; j++) {
+                fileCopy[lineNumber * maxLineSize + j] = '\0';
+            }
+
+            for (int j = 0; j < strlen(line); j++) {
+                fileCopy[lineNumber * maxLineSize + j] = line[j];
+//                printf("%c", line[j]);
+//                fflush(stdout);
+            }
+            lineNumber++;
+        }
+        fclose(inputFile);
+    }
 
     struct configuration conf;
 
     MPI_Barrier(MPI_COMM_WORLD);
 
-//    MPI_Bcast((void *) (fc->content), maxLineSize * fc->totalNumberOfLines, MPI_CHAR, ROOT, MPI_COMM_WORLD);
+    MPI_Bcast((void *) (fileCopy), maxLineSize * totalNumberOfLines, MPI_CHAR, ROOT, MPI_COMM_WORLD);
 
     MPI_Barrier(MPI_COMM_WORLD);
 
     for (int i = 0; i < 4; i++) {
         int tokenCount = 0;
-        char *rest = &(fc->content)[i * maxLineSize];
+        char *rest = &fileCopy[i * maxLineSize];
         while ((token = strtok_r(rest, space, &rest))) {
             switch (i) {
                 case 0:
-                    conf.numberOfIterations = atoi(token);
+                    numberOfIterations = atoi(token);
                     break;
                 case 1:
-                    conf.convergenceCoefficient = strtod(token, &token);
+                    convergenceCoefficient = strtod(token, &token);
                     break;
                 case 2:
-                    conf.numberOfFeatures = atoi(token);
+                    numberOfFeatures = atoi(token);
                     break;
                 case 3:
                     switch (tokenCount) {
                         case 0:
-                            conf.numberOfUsers = atoi(token);
+                            numberOfUsers = atoi(token);
                             break;
                         case 1:
-                            conf.numberOfItems = atoi(token);
+                            numberOfItems = atoi(token);
                             break;
                         case 2:
-
-                            conf.numberOfNonZeroElements = atoi(token);
+                            numberOfNonZeroElements = atoi(token);
                             break;
                     }
                     break;
@@ -78,19 +104,19 @@ void readInput(const char inputFileName[], const int processId, const int number
         }
     }
 
-    MatrixA = (double *) malloc(conf.numberOfUsers * conf.numberOfItems * sizeof(double));
+    MatrixA = (double *) malloc(numberOfUsers * numberOfItems * sizeof(double));
 
-    for (int l = 0; l < conf.numberOfUsers; l++) {
-        for (int i = 0; i < conf.numberOfItems; i++) {
-            MatrixA[l * conf.numberOfItems + i] = (double) 0;
+    for (int l = 0; l < numberOfUsers; l++) {
+        for (int i = 0; i < numberOfItems; i++) {
+            MatrixA[l * numberOfItems + i] = (double) 0;
         }
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
 
-    for (int k = 4; k < fc->totalNumberOfLines; k++) {
+    for (int k = 4; k < totalNumberOfLines; k++) {
         int tokenCount = 0;
-        char *rest = &(fc->content)[k * maxLineSize];
+        char *rest = &fileCopy[k * maxLineSize];
 
         int userIndex;
         int itemIndex;
@@ -110,14 +136,14 @@ void readInput(const char inputFileName[], const int processId, const int number
             }
             tokenCount++;
         }
-        MatrixA[userIndex * conf.numberOfItems + itemIndex] = nonZeroElement;
+        MatrixA[userIndex * numberOfItems + itemIndex] = nonZeroElement;
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
 
-    double *A = (double *) malloc(conf.numberOfUsers * conf.numberOfItems * sizeof(double));
+    double *A = (double *) malloc(numberOfUsers * numberOfItems * sizeof(double));
 
-    MPI_Reduce(MatrixA, A, conf.numberOfUsers * conf.numberOfItems, MPI_DOUBLE, MPI_MAX, ROOT, MPI_COMM_WORLD);
+    MPI_Reduce(MatrixA, A, numberOfUsers * numberOfItems, MPI_DOUBLE, MPI_MAX, ROOT, MPI_COMM_WORLD);
     MPI_Barrier(MPI_COMM_WORLD);
 
 //    if (processId == ROOT) {
@@ -132,7 +158,7 @@ void readInput(const char inputFileName[], const int processId, const int number
 //    }
     free(A);
     free(MatrixA);
-    free(fc);
+    free(fileCopy);
 
 }
 
